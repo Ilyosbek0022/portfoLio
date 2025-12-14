cc3fcf2428b3a0991d9dd360fece8c5c4593e05f
Ilyos Janoblari <ilyosbeknuriddinov14@gmail.com>
2025-12-14 13:00:26 +0500

    first commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,65 +1,73 @@
-import Image from "next/image";
+'use client'
+import Image from 'next/image'
+import { FaInstagram, FaTelegramPlane, FaGithub, FaFacebookF } from 'react-icons/fa'
+import React, { useEffect } from 'react'
 
-export default function Home() {
+const page = () => {
+  useEffect(() => {
+  const light = document.querySelector('.cursor-light')
+
+  const move = (e) => {
+    if (!light) return
+    light.style.left = e.clientX + 'px'
+    light.style.top = e.clientY + 'px'
+  }
+
+  window.addEventListener('mousemove', move)
+  return () => window.removeEventListener('mousemove', move)
+}, [])
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div>
+<div className="all">
+ 
+  <header>
+<div className="container">
+  <div className="between">
+    <div className="logo">
+   <Image
+      src="/logo.png"
+      alt="Landscape picture"
+      width={800}
+      height={500}
+    />
+    <h1>Ilyosbek</h1>
     </div>
-  );
+
+<div className="links">
+  <a href="#">about</a><a href="#">smth</a><a href="#">about</a><a href="#">contact</a><a href="#">My device</a>
+</div>
+
+<div className="social">
+  
+
+  <div className="social-icons">
+    <a href="#" aria-label="Instagram"><FaInstagram /></a>
+    <a href="#" aria-label="Telegram"><FaTelegramPlane /></a>
+    <a href="#" aria-label="GitHub"><FaGithub /></a>
+    <a href="#" aria-label="Facebook"><FaFacebookF /></a>
+  </div>
+</div>
+
+  </div>
+</div>
+  </header>
+   <div className="container">
+<main>
+  <div className="part1"><h1>Back-end</h1> <p>Ive no idea what Back-end is</p></div>
+  <div className="part2">
+  <img className="tilt-img" src="/me.png" alt="" />
+</div>
+
+  <div className="part3"><h1>Front-end</h1> <p>Ive no idea what Front-end is</p></div>
+</main>
+  </div>
+ 
+</div>
+<div className="cursor-light"></div>
+
+    </div>
+  )
 }
+
+export default page
